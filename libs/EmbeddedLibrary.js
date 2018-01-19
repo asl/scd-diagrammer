f8204ada35065a54dcaec31b6eb4e4c88615b622
Anton Korobeynikov <anton@korobeynikov.info>
2018-01-19 03:36:21 +0300

    Update 2018-01-19T00:36:18.756Z

---
+++ libs/EmbeddedLibrary.js
@@ -0,0 +1,40 @@
+EmbeddedLibrary = function(ui, data, title)  {
+    StorageFile.call(this, ui, data, title);
+};
+
+//Extends mxEventSource
+mxUtils.extend(EmbeddedLibrary, StorageFile);
+
+EmbeddedLibrary.prototype.getHash = function() {
+    return '<embedded: ' + this.title + '>';
+};
+
+EmbeddedLibrary.prototype.getTitle = function() {
+    return this.title;
+};
+
+EmbeddedLibrary.prototype.isAutosave = function() {
+    return false;
+};
+
+/**
+ * Overridden to avoid updating data with current file.
+ */
+EmbeddedLibrary.prototype.isEditable = function(title, success, error) {
+    return false;
+};
+
+/**
+ * Overridden to avoid updating data with current file.
+ */
+EmbeddedLibrary.prototype.saveAs = function(title, success, error)  {
+    // Cannot be saved
+};
+
+/**
+ * Returns the location as a new object.
+ * @type mx.Point
+ */
+EmbeddedLibrary.prototype.open = function() {
+    // Do nothing - this should never be called
+};
